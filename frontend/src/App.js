6b98857778a93cf86c81dbdcc687f41e774860ed
passwd10 <parkinseo94@naver.com>
2020-10-28 16:35:17 +0900

    feat: IssueList 페이지 컴포넌트 구조 설정
    UI 재활용할 수 있는 컴포넌트 단위 구성
    React 함수형 컴포넌트 정의 사용

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,81 @@
 import React from 'react';
 
+
+const IssueListNav = () => {
+  return (
+    <div>
+      이슈리스트메뉴
+    </div>
+  )
+}
+
+const SearchInput = () => {
+  return (
+    <div>
+      searchInput
+    </div>
+  )
+}
+
+const PropertyButton = () => {
+  return (
+    <div>
+      propertybutton
+    </div>
+  )
+}
+
+const NewIssueButton = () => {
+  return (
+    <div>
+      newIssueButton
+    </div>
+  )
+}
+
+const SearchBar = () => {
+  return (
+    <div>
+      <SearchInput/>
+      <PropertyButton/>
+      <NewIssueButton/>
+    </div>
+  )
+}
+
+const IssueListItem = () => {
+  return (
+    <div>
+      Issue1
+    </div>
+  )
+}
+
+const IssueList = () => {
+  return (
+    <div>
+      <IssueListItem />
+    </div>
+  )
+}
+
+const IssueListPage = () => {
+  return (
+    <div>
+      <SearchBar />
+      <IssueListNav />
+      <IssueList />
+    </div>
+  );
+}
+
+
 const App = () => {
   return (
-    <div>Hello World!</div>
+    <div>
+      <header>ISSUES</header>
+      <IssueListPage/>
+    </div>
   )
 }
 

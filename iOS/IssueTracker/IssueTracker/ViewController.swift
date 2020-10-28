e90369553dd9325392403969f7014b62d53f1140
mike123789-dev <mike123789-dev>
2020-10-28 14:02:32 +0900

    feat: iOS 프로젝트 생성

---
+++ iOS/IssueTracker/IssueTracker/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IssueTracker
+//
+//  Created by 강병민 on 2020/10/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

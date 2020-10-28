69ba5b030f0402f2cfd2b649dc532a201785e2ac
passwd10 <parkinseo94@naver.com>
2020-10-28 15:59:06 +0900

    feat: 프론트엔드 개발환경 세팅
    babel, webpack, eslint, react 환경구성

---
+++ frontend/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/index.js',
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/,
+      use: 'babel-loader',
+      exclude: /node_modules/,
+    }, {
+      test: /\.css$/,
+      use: ['style-loader'],
+      exclude: /node_modules/,
+    }],
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    publicPath: '/',
+    overlay: true,
+    stats: 'errors-only',
+    historyApiFallback: true,
+  },
+  devtool: 'cheap-eval-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })],
+};

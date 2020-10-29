2e69daf2b359f19eed9ac93085e8a53afb040186
ashtapor25 <junsushin4546@gmail.com>
2020-10-29 16:41:33 +0900

    feat: LabelsPage, MilestonesPage 컴포넌트 추가
    Labels, Milestones 버튼 클릭 시 라우팅 기능을 위해서 예시 페이지 생성

---
+++ frontend/src/pages/LabelsPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { getLabels } from '../apis/labelsAPI';
+
+const LabelsPage = () => {
+  const labels = getLabels();
+
+  return (
+    <div>Labels Page {labels.length}</div>
+  )
+}
+
+export default LabelsPage;

3047e269dfb40ae237e348243697e08f60e009b3
ashtapor25 <junsushin4546@gmail.com>
2020-10-29 11:33:47 +0900

    feat: SPA를 위해서 App 컴포넌트 구조 변경
    헤더를 제외한 영역이 라우팅으로 변경되기 때문에 Router 컴포넌트로 영역 추가
    페이지별 라우팅을 위해서 Switch와 Route 태그 추가하여 개별 페이지를 넣음

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import IssueListPage from './pages/IssueListPage'
+import IssueListPage from './pages/IssueListPage';
+import NewIssuePage from './pages/NewIssuePage';
 
 const App = () => {
   return (
     <div>
       <header>ISSUES</header>
-      <IssueListPage/>
+      <Router>
+        <Switch>
+          <Route path='/new'>
+            <NewIssuePage />
+          </Route>
+          <Route path='/'>
+            <IssueListPage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   )
 }

de641a66608fad47681f6678597696eac9c15a7c
passwd10 <parkinseo94@naver.com>
2020-10-29 11:13:54 +0900

    feat: IssueListNav에 필요한 컴포넌트 정의
    - IssueListNavButton 컴포넌트 정의
    - IssueListCheckBox 컴포넌트 정의

--- frontend/src/components/IssueListNav.jsx
+++ frontend/src/components/IssueListNav.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
+import IssueListNavButton from './IssueListNavButton';
+import IssueListCheckBox from './IssueListCheckBox';
+
 const IssueListNav = () => {
   return (
     <div>
       이슈리스트메뉴
+      <IssueListCheckBox />
+      <IssueListNavButton title={'Author'}/>
+      <IssueListNavButton title={'Label'}/>
+      <IssueListNavButton title={'Projects'}/>
+      <IssueListNavButton title={'Milestones'}/>
+      <IssueListNavButton title={'Assignee'}/>
+      <IssueListNavButton title={'Sort'}/>
     </div>
   )
 }
---
+++ frontend/src/components/IssueListNavButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const IssueListNavButton = ({ title }) => {
+  return (
+    <div>
+      [{title}]
+    </div>
+  )
+}
+
+export default IssueListNavButton;

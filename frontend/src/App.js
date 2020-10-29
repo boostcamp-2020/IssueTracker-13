7751d36838f2a2f70476e3e3ae449660ed36c258
ashtapor25 <junsushin4546@gmail.com>
2020-10-29 16:55:34 +0900

    feat: /milestones, /labels 라우트 추가
    레이블, 마일스톤 화면 라우트 추가 밑 예시 컴포넌트 렌더링 설정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import IssueListPage from './pages/IssueListPage';
+import MilestonesPage from './pages/MilestonesPage';
 import NewIssuePage from './pages/NewIssuePage';
+import LabelsPage from './pages/LabelsPage';
 
-const App = () => {
+const App = () => {  
   return (
     <div>
       <header>ISSUES</header>
@@ -12,6 +14,12 @@ const App = () => {
         <Switch>
           <Route path='/new'>
             <NewIssuePage />
+          </Route>
+          <Route path='/milestones'>
+            <MilestonesPage />
+          </Route>
+          <Route path='/labels'>
+            <LabelsPage />
           </Route>
           <Route path='/'>
             <IssueListPage />

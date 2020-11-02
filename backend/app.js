3bd91a1bde38aa83a3523fa5c7f8c25c96352b0e
passwd10 <parkinseo94@naver.com>
2020-11-02 22:02:30 +0900

    style: 불필요한 코드 제거
    - 불필요한 error처리코드 제거

--- backend/app.js
+++ backend/app.js
@@ -3,8 +3,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./src/routes/index');
-const usersRouter = require('./src/routes/users');
+const router = require('./src/routes');
 
 const app = express();
 
@@ -13,25 +12,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', router);
 
-console.log('listening');
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.use((req, res) => {
+  res.status(404).send('error');
 });
 
 module.exports = app;

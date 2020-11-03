68da47fd151755cc32793601bda2c5df73745ca9
passwd10 <parkinseo94@naver.com>
2020-11-03 20:26:58 +0900

    feat: comment router 추가

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,11 +6,13 @@ const issueRouter = require('./issues');
 const labelRouter = require('./labels');
 const milestoneRouter = require('./milestones');
 const userRouter = require('./users');
+const commentRouter = require('./comments');
 
 router.use('/auth', authRouter);
 router.use('/issues', issueRouter);
 router.use('/labels', labelRouter);
 router.use('/milestones', milestoneRouter);
 router.use('/users', userRouter);
+router.use('/comments', commentRouter);
 
 module.exports = router;

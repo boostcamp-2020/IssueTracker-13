cbb7e6874a47f7bf11a838b8d45504cf16e7eeea
passwd10 <parkinseo94@naver.com>
2020-11-03 11:43:27 +0900

    style: PR review 반영
    - base url을 /api로 설정
    - error handling code 추가

--- backend/app.js
+++ backend/app.js
@@ -12,14 +12,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', router);
+app.use('/api', router);
 
 app.use((req, res, next) => {
   next(createError(404));
 });
 
-app.use((req, res) => {
-  res.status(404).send('error');
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 module.exports = app;

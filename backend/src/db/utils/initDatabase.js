d0b91683b28905de4a7d3db9048f59cecbb4a5fd
junsushin-dev <junsushin4546@gmail.com>
2020-11-05 01:18:34 +0900

    refactor: initDatabase 모듈이 함수를 반환하도록 변경
    함수를 export 해서 npm script와 jest에 범용적으로 사용가능하도록 변경
    테스트 시에 불필요한 로그가 없도록 로그 출력 여부 인수 추가
    테스트 시에 connection을 열어놓도록 작업 완료 후 close 여부 인수 추가

--- backend/src/db/utils/initDatabase.js
+++ backend/src/db/utils/initDatabase.js
@@ -1,14 +1,17 @@
-const db = require('../models');
+const path = require('path');
 const { Umzug, SequelizeStorage, memoryStorage } = require('umzug');
+
+const db = require('../models');
+
 const sequelize = db.sequelize;
 
 const disableLogger = (sequelize) => {
   sequelize.options.logging = false;
 };
 
-const migrations = new Umzug({
+const getMigrations = (logger) => new Umzug({
   migrations: {
-    glob: 'src/db/migrations/*.js',
+    glob: path.resolve(__dirname, '../migrations/*.js'),
     resolve: ({ name, path, context }) => {
       const migration = require(path);
       return {
@@ -20,12 +23,12 @@ const migrations = new Umzug({
   },
   context: [sequelize.getQueryInterface(), db.Sequelize],
   storage: new SequelizeStorage({ sequelize }),
-  logger: console,
+  logger,
 });
 
-const seeders = new Umzug({
+const getSeeders = (logger) => new Umzug({
   migrations: {
-    glob: 'src/db/seeders/*.js',
+    glob: path.resolve(__dirname, '../seeders/*.js'),
     resolve: ({ name, path, context }) => {
       const migration = require(path);
       return {
@@ -37,13 +40,19 @@ const seeders = new Umzug({
   },
   context: [sequelize.getQueryInterface(), db.Sequelize],
   storage: memoryStorage(),
-  logger: console,
+  logger,
 });
 
-(async () => {
+const initDatabase = async ({ logger, closeAfter } = { logger: console, closeAfter: true }) => {
   disableLogger(sequelize);
+  const migrations = getMigrations(logger);
+  const seeders = getSeeders(logger);
   await migrations.down({ to: 0 });
   await migrations.up();
   await seeders.up();
-  sequelize.close();
-})();
+  if (closeAfter) {
+    sequelize.close();
+  }
+};
+
+module.exports = initDatabase;

4b68d9e016525d08a84ff9b752134070ccd44dbb
mike123789-dev <mike123789@g.skku.edu>
2020-11-04 14:22:43 +0900

    feat: 필터레이블 화면, 필터마일스톤 화면 생성

---
+++ iOS/IssueTracker/IssueTracker/Scenes/FilterIssue/FilterMilestoneListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FilterMilestoneListViewController.swift
+//  IssueTracker
+//
+//  Created by 강병민 on 2020/11/04.
+//
+
+import UIKit
+
+class FilterMilestoneListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}

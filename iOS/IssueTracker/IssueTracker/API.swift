cba0d911fa0f599d6f9301aba741105dce8dac79
mike123789-dev <mike123789@g.skku.edu>
2020-11-05 18:38:15 +0900

    feat: 네트워킹을 위한 API 클래스 추가
    Alamofire 이용

--- iOS/IssueTracker/IssueTracker/API.swift
+++ iOS/IssueTracker/IssueTracker/API.swift
@@ -12,12 +12,38 @@ class API {
     static let shared: API = API()
     
     func getLabels(completion : @escaping (Result<[Label], Error>) -> Void) {
-        AF.request("http://27.96.130.188:3000/api/labels")
+        AF.request("http://27.96.130.188:3000/api/labels", method: .get)
             .validate()
             .responseDecodable(of: [Label].self) { (response) in
                 switch response.result {
                 case .success(let labels):
                     completion(.success(labels))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+    
+    func postLabel(_ postLabel: PostLabel, completion : @escaping (Result<String, Error>) -> Void) {
+        AF.request("http://27.96.130.188:3000/api/labels", method: .post, parameters: postLabel)
+            .validate()
+            .responseDecodable(of: ReponseMessage.self) { (response) in
+                switch response.result {
+                case .success(let message):
+                    completion(.success(message.message))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+    
+    func putLabel(_ putLabel: PutLabel, completion : @escaping (Result<String, Error>) -> Void) {
+        AF.request("http://27.96.130.188:3000/api/labels", method: .put, parameters: putLabel)
+            .validate()
+            .responseDecodable(of: ReponseMessage.self) { (response) in
+                switch response.result {
+                case .success(let message):
+                    completion(.success(message.message))
                 case .failure(let error):
                     completion(.failure(error))
                 }
@@ -31,6 +57,32 @@ class API {
                 switch response.result {
                 case .success(let milestones):
                     completion(.success(milestones))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+    
+    func postMilestone(_ postMilestone: PostMilestone, completion : @escaping (Result<String, Error>) -> Void) {
+        AF.request("http://27.96.130.188:3000/api/milestones", method: .post, parameters: postMilestone)
+            .validate()
+            .responseDecodable(of: ReponseMessage.self) { (response) in
+                switch response.result {
+                case .success(let message):
+                    completion(.success(message.message))
+                case .failure(let error):
+                    completion(.failure(error))
+                }
+            }
+    }
+    
+    func putMilestone(_ putMilestone: PutMilestone, completion : @escaping (Result<String, Error>) -> Void) {
+        AF.request("http://27.96.130.188:3000/api/milestones", method: .put, parameters: putMilestone)
+            .validate()
+            .responseDecodable(of: ReponseMessage.self) { (response) in
+                switch response.result {
+                case .success(let message):
+                    completion(.success(message.message))
                 case .failure(let error):
                     completion(.failure(error))
                 }
--- iOS/IssueTracker/IssueTracker/ReponseMessage.swift
+++ iOS/IssueTracker/IssueTracker/ReponseMessage.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+struct ReponseMessage: Codable {
+    let message: String
+}

3c91ed1c99d574d27545904b1bbfff08d2ced1f4
parkdit94 <dw1156@naver.com>
2020-11-05 16:15:17 +0900

    feat: users의 routes 정의

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,8 +1,48 @@
 const express = require('express');
 const router = express.Router();
+const createError = require('http-errors');
 
-router.get('/', (req, res) => {
-  res.json();
+const { getUsers, addUser, updateUser, deleteUser } = require('../services/userService');
+
+const SUCCESS_MESSAGE = { message: 'success' };
+
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await getUsers();
+    res.json(users);
+  } catch (error) {
+    next(createError(500));
+  }
+});
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newUser = req.body;
+    await addUser(newUser);
+    res.send(SUCCESS_MESSAGE);
+  } catch (error) {
+    next(createError(500));
+  }
+});
+
+router.put('/', async (req, res, next) => {
+  try {
+    const modifiedContents = req.body;
+    await updateUser(modifiedContents);
+    res.send(SUCCESS_MESSAGE);
+  } catch (error) {
+    next(createError(500));
+  }
+});
+
+router.delete('/', async (req, res, next) => {
+  try {
+    const { id } = req.body;
+    await deleteUser(id);
+    res.send(SUCCESS_MESSAGE);
+  } catch (error) {
+    next(createError(500));
+  }
 });
 
 module.exports = router;

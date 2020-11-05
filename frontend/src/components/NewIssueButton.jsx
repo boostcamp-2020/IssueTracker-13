54b875d3c8f1be9efbfc947d0c0547c7ade32560
passwd10 <parkinseo94@naver.com>
2020-11-05 14:40:00 +0900

    feat: NewIssueButton styling

--- frontend/src/components/NewIssueButton.jsx
+++ frontend/src/components/NewIssueButton.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import styled from 'styled-components';
+
+const Button = styled.button`
+  padding: 8px 12px;
+  background-color: #28a745;
+  font-weight: bold;
+  color: white;
+  border: 1px solid #e1e4e8;
+  border-radius: 10%;
+`;
+
 const NewIssueButton = () => {
   return (
     <Link to='/new'>
-      New issue
+      <Button>New issue</Button>
     </Link>
   );
 };

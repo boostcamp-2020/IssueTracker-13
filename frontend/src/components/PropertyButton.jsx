eb2d6538ab9471476c7a1749ea696df778c1393d
passwd10 <parkinseo94@naver.com>
2020-11-05 14:36:17 +0900

    feat: Property Button styling

--- frontend/src/components/PropertyButton.jsx
+++ frontend/src/components/PropertyButton.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const PropertyButton = ({ route, text, img, count }) => {
+import styled from 'styled-components';
+
+const Button = styled.button`
+  padding: 8px 12px;
+  background-color: white;
+  font-weight: bold;
+  border: 1px solid #d1d5da;
+`;
+
+const Count = styled.span`
+  padding: 3px 7px;
+  border: 1px solid #e1e4e8;
+  background-color: #e1e4e8;
+  border-radius: 50%;
+`;
+
+export default function PropertyButton({ route, text, img, count }) {
   return (
     <Link to={route}>
-      <img src={img}></img>
-      {text}
-      <span>{count}</span>
+      <Button>
+        <img src={img}></img>
+        {text}
+        <Count>{count}</Count>
+      </Button>
     </Link>
   );
-};
+}
 
-export default PropertyButton;

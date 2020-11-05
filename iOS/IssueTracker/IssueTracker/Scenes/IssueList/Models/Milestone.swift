0a5097244132dd13bb03113adb863be88c931cf1
mike123789-dev <mike123789@g.skku.edu>
2020-11-05 15:29:56 +0900

    feat: Milestone struct 추가

---
+++ iOS/IssueTracker/IssueTracker/Scenes/IssueList/Models/Milestone.swift
@@ -0,0 +1,15 @@
+//
+//  Milestone.swift
+//  IssueTracker
+//
+//  Created by 강병민 on 2020/11/05.
+//
+
+import Foundation
+
+struct Milestone: Codable {
+    let id: Int
+    let title, description, dueDate: String
+    let isDeleted: Bool
+    let createdAt, updatedAt: String
+}

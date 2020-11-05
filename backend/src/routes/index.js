2ede73e5a59e31e722e48f8fd9871e82ed6a877a
parkdit94 <dw1156@naver.com>
2020-11-05 18:41:54 +0900

    fix: 주석 처리 제거

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-//const authRouter = require('./auth');
+const authRouter = require('./auth');
 const issueRouter = require('./issues');
 const labelRouter = require('./labels');
 const milestoneRouter = require('./milestones');
 const userRouter = require('./users');
 const commentRouter = require('./comments');
 
-//router.use('/auth', authRouter);
+router.use('/auth', authRouter);
 router.use('/issues', issueRouter);
 router.use('/labels', labelRouter);
 router.use('/milestones', milestoneRouter);

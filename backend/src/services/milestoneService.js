4780726c2822f2c8256834133ff388917572c09c
passwd10 <parkinseo94@naver.com>
2020-11-06 11:16:59 +0900

    feat: milestone GET API에 정보 추가
    - 마일스톤과 관련된 모든 이슈의 개수
    - 마일스톤과 관련된 닫힌 이슈의 개수

--- backend/src/services/milestoneService.js
+++ backend/src/services/milestoneService.js
@@ -1,13 +1,51 @@
-const { Milestone } = require('../db/models');
+const { Milestone, Issue } = require('../db/models');
+
+// 마일스톤아이디에 해당되는 전체 issue 개수
+const getAllIssueCountAboutMilestone = async (milestoneId) => {
+  const allIssueCount = await Issue.count({
+    where: {
+      MilestoneId: milestoneId,
+      isDeleted: false,
+    },
+  });
+
+  return allIssueCount;
+};
+
+// 마일스톤아이디에 해당되는 완료된 issue 개수
+const getClosedIssueCountAboutMilestone = async (milestoneId) => {
+  const cloesdIssueCount = Issue.count({
+    where: {
+      MilestoneId: milestoneId,
+      isOpen: false,
+      isDeleted: false,
+    },
+  });
+
+  return cloesdIssueCount;
+};
+
+const addIssueInfoInMilestone = async (milestones) => {
+  return await Promise.all(
+    milestones.map(async(milestone) => {
+      const allIssueCount = await getAllIssueCountAboutMilestone(milestone.id);
+      const closedIssueCount = await getClosedIssueCountAboutMilestone(milestone.id);
+
+      return { ...milestone, allIssueCount: allIssueCount, closedIssueCount: closedIssueCount };
+    }),
+  );
+};
 
 const getMilestones = async () => {
   const milestones = await Milestone.findAll({
+    raw: true,
     where: {
       isDeleted: false,
     },
     attributes: ['id', 'title', 'description', 'dueDate'],
   });
-  return milestones;
+
+  return await addIssueInfoInMilestone(milestones);
 };
 
 const addMilestone = async (newMilestone) => {

be2e7658aaba8f68b359e902d86bca99d1b6d996
mike123789-dev <mike123789@g.skku.edu>
2020-11-08 10:22:59 +0900

    feat: LablerAlertVC 기능 추가
    - 랜덤 RGB값 생성기능 추가
    - 정상적인 RGB값만 저정되도록 개선

--- iOS/IssueTracker/IssueTracker/Scenes/Alert/LabelAlertViewController.swift
+++ iOS/IssueTracker/IssueTracker/Scenes/Alert/LabelAlertViewController.swift
@@ -15,6 +15,7 @@ class LabelAlertViewController: BaseAlertViewController {
     @IBOutlet weak var alertView: CustomAlertView!
     @IBOutlet var colorView: UIView!
     @IBOutlet weak var colorTextField: UITextField!
+    @IBOutlet weak var colorPickerView: UIView!
     
     weak var delegate: LabelAlertDelegate?
     
@@ -24,17 +25,59 @@ class LabelAlertViewController: BaseAlertViewController {
         alertView.closeButton.addTarget(self, action: #selector(didTouchCloseButton), for: .touchUpInside)
         alertView.resetButton.addTarget(self, action: #selector(didTouchResetButton), for: .touchUpInside)
         alertView.saveButton.addTarget(self, action: #selector(didTouchSaveButton), for: .touchUpInside)
+        colorTextField.addTarget(self, action: #selector(didTextFieldChange(_:)), for: .editingChanged)
     }
-
+    
     func configure(_ mode: AlertMode, label: Label?) {
+        self.mode = mode
         switch mode {
         case .edit:
             self.id = label?.id
             alertView.titleTextField.text = label?.title
             alertView.descriptionTextField.text = label?.description
-            colorTextField.text = label?.color
+            
+            guard let backgroundColorString = label?.backgroundColor else { return }
+            colorTextField.text = backgroundColorString
+            colorPickerView.backgroundColor = UIColor(hexString: backgroundColorString)
         default:
             break
+        }
+    }
+    
+    func isValidate(_ string: String) -> Bool {
+        let otherChars = string.dropFirst()
+        for char in otherChars where !char.isHexDigit {
+            return false
+        }
+        return true
+    }
+    
+    @IBAction func didTouchRandomButton(_ sender: Any) {
+        colorPickerView.backgroundColor = generateRandomColor()
+        colorTextField.text = colorPickerView.backgroundColor?.toHexString()
+    }
+    
+    func generateRandomColor() -> UIColor {
+        return UIColor(red: .random(in: 0...1),
+                       green: .random(in: 0...1),
+                       blue: .random(in: 0...1),
+                       alpha: 1.0)
+    }
+    
+    @objc func didTextFieldChange(_ textField: UITextView) {
+        guard let colorString = textField.text else { return }
+        
+        if colorString.isEmpty {
+            textField.text = "#"
+        } else if colorString.count == 7 {
+            if isValidate(colorString) {
+                colorPickerView.backgroundColor = UIColor(hexString: colorString)
+                alertView.saveButton.isEnabled = true
+            } else {
+                alertView.saveButton.isEnabled = false
+            }
+        } else {
+            alertView.saveButton.isEnabled = false
         }
     }
     
@@ -50,23 +93,23 @@ class LabelAlertViewController: BaseAlertViewController {
     
     @objc func didTouchSaveButton() {
         guard let mode = mode else { return }
+        guard let color = colorTextField.text else { return }
+        let backgroundColor = generateTextColor()
         let label: Labelable
         switch mode {
         case .add:
-            let backgroundColor = ""
-            let color = calculateColor(with: backgroundColor)
             label = PostLabel(title: alertView.titleTextField.text, description: alertView.descriptionTextField.text, color: color, backgroundColor: backgroundColor)
         case .edit:
-            let backgroundColor = ""
-            let color = calculateColor(with: backgroundColor)
-            label = PutLabel(id: 1, title: alertView.titleTextField.text, description: alertView.descriptionTextField.text, color: color, backgroundColor: backgroundColor)
-        default:
-            break
+            guard let id = id else { return }
+            label = PutLabel(id: id, title: alertView.titleTextField.text, description: alertView.descriptionTextField.text, color: color, backgroundColor: backgroundColor)
         }
+        
         delegate?.didTouchSaveButton(label: label, mode: mode)
     }
     
-    func calculateColor(with backgroundColor: String) -> String {
-        return ""
+    func generateTextColor() -> String {
+        guard let isDark = colorPickerView.backgroundColor?.isDarkColor else { return "#000000"}
+        return isDark ? "#ffffff" : "#000000"
     }
+    
 }

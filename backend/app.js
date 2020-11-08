f3d49f8523331dd675066122de1855e5c5768143
junsushin-dev <junsushin4546@gmail.com>
2020-11-08 17:09:36 +0900

    feat: Express에서 정적 파일 제공폴더 추가
    Nginx 도입 이전 Express를 통해서 프론트엔드 빌드파일 제공 목적

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const createError = require('http-errors');
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const path = require('path');
 
 const router = require('./src/routes');
 
@@ -13,6 +14,7 @@ app.use(logger('dev', {
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.resolve(__dirname, '../frontend/dist')));
 
 app.use('/api', router);
 

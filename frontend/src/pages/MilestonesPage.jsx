97efc7718a9d7d3d83018d631cea70291e9bf6da
junsushin-dev <junsushin4546@gmail.com>
2020-11-10 22:08:37 +0900

    feat: MilestonesPage 구조변경 및 상태관리 추가
    Header, ListNav, List 단위 구조 추가
    milestones 정보 상태관리 추가

--- frontend/src/pages/MilestonesPage.jsx
+++ frontend/src/pages/MilestonesPage.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+
+import MilestoneList from '../components/MilestoneList';
+
+import { getMilestones } from '../apis/milestonesAPI';
+
+const Page = styled.div`
+  padding: 48px 200px;
+`;
 
 export default function MilestonesPage() {
+  const [milestones, setMilestones] = useState([]);
+
+  useEffect(async () => {
+    const newMilestones = await getMilestones();
+    setMilestones(newMilestones);
+  });
+
   return (
-    <div>Milestones Page</div>
+    <Page>
+      {/* <MilestoneHeader/> */}
+      {/* <MilestoneListNav openCount={milestones.length} closedCount={0}/> */}
+      <MilestoneList milestones={ milestones }/>
+    </Page>
   );
 }

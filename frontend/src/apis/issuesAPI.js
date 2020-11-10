61e7041c5a1c2a128a0756d82cb14e9a384fe9fe
passwd10 <parkinseo94@naver.com>
2020-11-10 16:48:22 +0900

    feat: mock-data 대신 API 사용

--- frontend/src/apis/issuesAPI.js
+++ frontend/src/apis/issuesAPI.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const getIssues = async (query) => {
-  const { data } = await axios.get(`/api/issues${query}`);
+  const { data } = await axios.get(`/api/issues?${query}`);
+  console.log('query : ', query);
   return data.map(issue => {
     return { ...issue,
       author: issue.author.userName,

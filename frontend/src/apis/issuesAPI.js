7f97cacfd1a1cb573fca6767c99f65649cc50d70
passwd10 <parkinseo94@naver.com>
2020-11-10 21:56:18 +0900

    chore: 불필요한 console 제거

--- frontend/src/apis/issuesAPI.js
+++ frontend/src/apis/issuesAPI.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const getIssues = async (query) => {
   const { data } = await axios.get(`/api/issues?${query}`);
-  console.log('query : ', query);
   return data.map(({ id, isOpen, preview, title, createdAt, Assignee, Labels, Milestone, author }) => {
     return {
       id,

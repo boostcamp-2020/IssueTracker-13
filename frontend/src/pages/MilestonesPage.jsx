bb8104b6bb76702f2bcc6b1ff9370d4a6c9ae621
junsushin-dev <junsushin4546@gmail.com>
2020-11-10 23:57:20 +0900

    feat: LabelMilestoneHeader 컴포넌트 추가
    Labels 페이지와 Milestones 페이지 헤더가 동일하여 재사용 컴포넌트로 구현

--- frontend/src/pages/MilestonesPage.jsx
+++ frontend/src/pages/MilestonesPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
+import LabelMilestoneHeader from '../components/LabelMilestoneHeader';
 import MilestoneListNav from '../components/MilestoneListNav';
 import MilestoneList from '../components/MilestoneList';
 
@@ -20,7 +21,7 @@ export default function MilestonesPage() {
 
   return (
     <Page>
-      {/* <MilestoneHeader/> */}
+      <LabelMilestoneHeader/>
       <MilestoneListNav milestones={ milestones }/>
       <MilestoneList milestones={ milestones }/>
     </Page>

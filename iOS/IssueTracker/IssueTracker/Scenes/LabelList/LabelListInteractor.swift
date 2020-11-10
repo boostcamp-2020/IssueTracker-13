ee701720d31ce7888212591e3c32bb36c9f5b38b
mike123789-dev <mike123789@g.skku.edu>
2020-11-10 14:17:01 +0900

    feat: 라벨 목록 네트워킹 연동
    수정, 추가 기능 구현

--- iOS/IssueTracker/IssueTracker/Scenes/LabelList/LabelListInteractor.swift
+++ iOS/IssueTracker/IssueTracker/Scenes/LabelList/LabelListInteractor.swift
@@ -18,7 +18,53 @@ class LabelListInteractor: LabelListBusinessLogic {
     
     func fetchLabelList() {
         worker.fetchLabels { (datasource) in
+            self.issueDataSource = datasource
             self.viewController?.displayLabelList(with: datasource.labels, at: .main)
+        }
+    }
+    
+    func add(label: PostLabel) {
+        guard let dataSource = issueDataSource else { return }
+        let newID = dataSource.labels.count + 1
+        let newLabel = Label(postLabel: label, id: newID)
+        dataSource.add(label: newLabel)
+        viewController?.displayLabelList(with: dataSource.labels, at: .main)
+    }
+    
+    func update(label: Label) {
+        guard let dataSource = issueDataSource else { return }
+        dataSource.update(label: label)
+        viewController?.displayLabelList(with: dataSource.labels, at: .main)
+    }
+    
+}
+
+extension LabelListInteractor: LabelAlertDelegate {
+    
+    func didTouchSaveButton(label: Labelable, mode: AlertMode) {
+        switch mode {
+        case .add:
+            guard let label = label as? PostLabel else { return }
+
+            API.shared.post(data: label, to: .labels, completion: { (result) in
+                switch result {
+                case .success:
+                    self.add(label: label)
+                case .failure:
+                    print("failure")
+                }
+            })
+
+        case .edit:
+            guard let label = label as? Label else { return }
+            API.shared.put(data: label, to: .labels, completion: { (result) in
+                switch result {
+                case .success:
+                    self.update(label: label)
+                case .failure:
+                    print("failure")
+                }
+            })
         }
     }
     

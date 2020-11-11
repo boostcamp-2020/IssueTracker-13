a3175278ddad09d1eb8f78e67e566065178b3663
jaejeon <jaejeon@student.42seoul.kr>
2020-11-11 17:40:45 +0900

    feat: Issue 상세 화면 Bottom Sheet 레이아웃 작업 완료

--- iOS/IssueTracker/IssueTracker/Scenes/IssueDetail/IssueDetailViewController.swift
+++ iOS/IssueTracker/IssueTracker/Scenes/IssueDetail/IssueDetailViewController.swift
@@ -10,9 +10,7 @@ import FloatingPanel
 
 class IssueDetailViewController: UIViewController {
     var issue: Issue?
-    var bottomSheet: FloatingPanelController {
-        return FloatingPanelController()
-    }
+    var bottomSheet = FloatingPanelController()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,7 +39,9 @@ extension IssueDetailViewController: FloatingPanelControllerDelegate {
         bottomSheet.delegate = self // Optional
 
         // Set a content view controller.
-        let contentVC = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(withIdentifier: "IssueBottomSheetViewController") as? IssueBottomSheetViewController
+        guard let contentVC = UIStoryboard(name: "Main", bundle: .main).instantiateViewController(identifier: "IssueBottomSheetViewController", creator: { (coder) in
+            return IssueBottomSheetViewController(coder: coder, issue: self.issue)
+        }) as? IssueBottomSheetViewController else { return }
         bottomSheet.set(contentViewController: contentVC)
 
         // Track a scroll view(or the siblings) in the content view controller.

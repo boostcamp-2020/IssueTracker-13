59086357b6dd8adc12b48c7e866f87f4b9871608
mike123789-dev <mike123789@g.skku.edu>
2020-11-12 00:42:08 +0900

    feat: 이슈목록화면에서 여러 라벨 보이는 stackview 구현

--- iOS/IssueTracker/IssueTracker/Scenes/IssueList/Views/IssueCollectionViewCell.swift
+++ iOS/IssueTracker/IssueTracker/Scenes/IssueList/Views/IssueCollectionViewCell.swift
@@ -13,16 +13,29 @@ class IssueCollectionViewCell: UICollectionViewListCell {
     @IBOutlet weak var previewLabel: UILabel!
     @IBOutlet weak var milestoneBadgeLabel: MilestoneBadgeLabel!
     @IBOutlet weak var labelBadgeLabel: LabelBadgeLabel!
+    @IBOutlet weak var labelStackView: UIStackView!
     
     func configure(with issue: Issue) {
         titleLabel.text = issue.title
         previewLabel.text = issue.preview.isEmpty ? " " : issue.preview
         milestoneBadgeLabel.configure(with: issue.milestone.title)
-        if let firstLabel = issue.labels.first {
-            labelBadgeLabel.configure(with: firstLabel)
-        }
+//        if let firstLabel = issue.labels.first {
+//            labelBadgeLabel.configure(with: firstLabel)
+//        }
+        configure(labelStackView, with: issue.labels)
+        print("\(issue.title) : \(issue.labels.count)")
+        
         separatorLayoutGuide.leadingAnchor.constraint(equalTo: titleLabel.leadingAnchor).isActive = true
         accessories = [.multiselect(displayed: .whenEditing, options: .init())]
+    }
+    
+    func configure(_ stackview: UIStackView, with labels: [Label]) {
+        labelStackView.subviews.forEach({$0.removeFromSuperview()})
+        labels.forEach { (label) in
+            let newLabel = LabelBadgeLabel()
+            newLabel.configure(with: label)
+            labelStackView.addArrangedSubview(newLabel)
+        }
     }
 
 }

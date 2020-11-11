d2525328b06e51598e8c2edeb9d928a4ec65777c
passwd10 <parkinseo94@naver.com>
2020-11-11 15:49:02 +0900

    feat: useParams를 이용하여 issueDetail API 요청
    - webpack에 publicpath를 설정해주자

--- frontend/src/pages/IssueDetailPage.jsx
+++ frontend/src/pages/IssueDetailPage.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 import IssueDetailHeader from '../components/IssueDetailHeader';
 import IssueDetailSidebar from '../components/IssueDetailSidebar';
 import CommentInput from '../components/CommentInput';
 import CommentList from '../components/CommentList';
 
+import { getIssueDetail } from '../apis/issuesAPI';
+
 const IssueDetailPage = () => {
+  const { id } = useParams();
+
+  const fetchIssueDetail = async (id) => {
+    const newIssue = await getIssueDetail(id);
+  };
+
+  useEffect(() => {
+    fetchIssueDetail(id);
+  }, []);
+
   return (
     <>
       <IssueDetailHeader />

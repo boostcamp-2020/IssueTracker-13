d427886eef44c259b9368d0572aa9b9575d07b3a
junsushin-dev <junsushin4546@gmail.com>
2020-11-12 11:52:06 +0900

    feat: 마일스톤 페이지 사용을 위한 API 호출함수 추가
    Milestone Open, Close, Delete 기능을 위한 함수들

--- frontend/src/apis/milestonesAPI.js
+++ frontend/src/apis/milestonesAPI.js
@@ -5,6 +5,32 @@ const getMilestones = async () => {
   return data;
 };
 
+const closeMilestone = async (id) => {
+  const requestBody = {
+    id,
+    isOpen: false,
+  };
+  const { message } = await axios.put('/api/milestones', requestBody);
+  return message;
+};
+
+const openMilestone = async (id) => {
+  const requestBody = {
+    id,
+    isOpen: true,
+  };
+  const { message } = await axios.put('/api/milestones', requestBody);
+  return message;
+};
+
+const deleteMilestone = async (id) => {
+  const { message } = await axios.delete(`/api/milestones/${id}`);
+  return message;
+};
+
 export {
   getMilestones,
+  closeMilestone,
+  deleteMilestone,
+  openMilestone,
 };

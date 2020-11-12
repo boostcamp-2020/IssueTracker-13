2ea58ae2788b784b052db7e7c58c63ea06b76042
junsushin-dev <junsushin4546@gmail.com>
2020-11-12 12:37:44 +0900

    feat: 로그인페이지 CSS 추가
    기본 스타일링 CSS 추가

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -11,28 +11,59 @@ const Page = styled.div`
 const Title = styled.h1``;
 
 const LoginBox = styled.div`
-  margin: 0 auto;
+  padding: 20px;
+  margin: 40px auto;
   width: 500px;
+  box-shadow: 0 0 3px gray;
 `;
 
 const LabelInputContainer = styled.label`
   display: flex;
+  margin-bottom: 30px;
   flex-flow: column nowrap;
   align-items: left;
   text-align: left;
+  gap: 10px;
 `;
 
-const InputLabel = styled.label``;
+const InputLabel = styled.label`
+  font-size: 20px;
+  font-weight: 600;
+`;
 
-const EmailInput = styled.input``;
+const EmailInput = styled.input`
+  height: 30px;
+`;
 
-const PasswordInput = styled.input``;
+const PasswordInput = styled.input`
+height: 30px;
+`;
 
-const RowContainer = styled.div``;
+const RowContainer = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+`;
 
-const Button = styled.button``;
+const Button = styled.button`
+  font-size: 16px;
+  font-weight: 600;
+  border: none;
+  color: #0366D6;
+  background-color: transparent;
+  cursor: pointer;
+`;
 
-const GitHubButton = styled.button``;
+const GitHubButton = styled.button`
+  color: white;
+  height: 40px;
+  font-size: 16px;
+  font-weight: 600;
+  margin-top: 30px;
+  width: 100%;
+  background-color: #888888;
+  border: none;
+  cursor: pointer;
+`;
 
 export default function LoginPage() {
   return (
